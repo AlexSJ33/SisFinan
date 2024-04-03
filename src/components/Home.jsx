ddc442b3b0049f3ef1d84b04d2eadc711b47b4c2
AlexSJ33 <alevildark@gmail.com>
2024-04-03 18:03:29 -0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+    <div className='container'>
+        <div className='item'>1</div>
+        <div className='item'>2</div>
+        <div className='item'>3</div>
+        <div className='item'>4</div>
+        <div className='item'>5</div>
+        <div className='item'>6</div>
+    </div>
+    </>
+  )
+}
+
+export default Home
