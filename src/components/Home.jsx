aae2eaeb1b260f610a6207f9a55fb0c8f4687198
AlexSJ33 <alevildark@gmail.com>
2024-04-30 13:13:10 -0300

    third commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,30 @@ function Home() {
     <>
     <div className='container'>
         <div className='item1'>
-          <div>Saldo</div>
-          <div>Receita</div>
-          <div>Despesa</div>
+          <div className='display saldo'>
+            <h2>Saldo</h2>
+            <p>9999,99</p>
+          </div>
+          <div className='display receita'>
+            <h2>Receita</h2>
+            <p>9999,99</p>
+          </div>
+          <div className='display despesa'>
+            <h2>Despesa</h2>
+            <p>9999,99</p>
+          </div>
         </div>
-        <div className='item2'>Menu</div>
+        <div className='item2'>
+          <div>LOGO</div>
+          <div className='botoes'>
+            <button className='btnReceita'>Nova Receita</button>
+            <button className='btnDespesa'>Nova Despesa</button>
+          </div>
+        </div>
         <div className='item3'>Tela1</div>
         <div className='item4'>Tela2</div>
         <div className='item5'>Tela3</div>
-        <div className='item6'>Tela4</div>
+        
         <div className='item7'>Footer</div>
     </div>
     </>
---
+++ src/components/Receita/AdicionarReceita.jsx
@@ -0,0 +1,12 @@
+import './AdicionarReceita.css'
+
+function AdicionarReceita() {
+    return (
+      <>
+      <div className='container'>
+          <div className='item1'>Tela1</div>
+      </div>
+      </>
+    )
+  }
+  export default AdicionarReceita
